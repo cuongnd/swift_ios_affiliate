9f33f0887133cc5c566bcd0d90102d2daa7509ee
cuongnd <nguyendinhcuong@gmail.com>
2020-12-21 21:25:54 +0700

    ipdate copde

--- FoodApp/Components/Ecommerce/Views/Home view/HomeVC.swift
+++ FoodApp/Components/Ecommerce/Views/Home view/HomeVC.swift
@@ -52,12 +52,13 @@ class HomeVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-              pieChartUpdate()
         
        
        
         let urlStringDoanhThuTheoThang = API_URL + "/api/affiliates/get_doanh_thu_theo_thang"
         self.Webservice_getDoanhThuTheoThang(url: urlStringDoanhThuTheoThang, params: [:])
+        let urlStringHieuQuaDonHang = API_URL + "//api/affiliates/get_hieu_qua_don_hang"
+        self.Webservice_getHieuQuaDonHang(url: urlStringHieuQuaDonHang, params: [:])
         
        
         
@@ -65,37 +66,6 @@ class HomeVC: UIViewController {
    }
     
        
-       func pieChartUpdate () {
-           
-           // Basic set up of plan chart
-           
-           let entry1 = PieChartDataEntry(value: Double(24), label: "#1")
-           let entry2 = PieChartDataEntry(value: Double(49), label: "#2")
-           let entry3 = PieChartDataEntry(value: Double(32), label: "#3")
-        let dataSet = PieChartDataSet(entries: [entry1, entry2, entry3], label: "Widget Types")
-           let data = PieChartData(dataSet: dataSet)
-           pieChart.data = data
-           pieChart.chartDescription?.text = "Share of Widgets by Type"
-
-           // Color
-           dataSet.colors = ChartColorTemplates.joyful()
-           //dataSet.valueColors = [UIColor.black]
-           pieChart.backgroundColor = UIColor.white
-           pieChart.holeColor = UIColor.clear
-           pieChart.chartDescription?.textColor = UIColor.black
-           pieChart.legend.textColor = UIColor.black
-           
-           // Text
-           pieChart.legend.font = UIFont(name: "Futura", size: 10)!
-           pieChart.chartDescription?.font = UIFont(name: "Futura", size: 12)!
-           pieChart.chartDescription?.xOffset = pieChart.frame.width
-           pieChart.chartDescription?.yOffset = pieChart.frame.height * (2/3)
-           pieChart.chartDescription?.textAlign = NSTextAlignment.left
-
-           // Refresh chart with new data
-           pieChart.notifyDataSetChanged()
-       }
-    
     
     @IBAction func GoToSearch(_ sender: UIButton) {
         let searchVC = UIStoryboard(name: "Products", bundle: nil).instantiateViewController(identifier: "SearchVC") as! SearchVC
@@ -162,6 +132,53 @@ extension HomeVC
             }
         }
     }
+    
+    func Webservice_getHieuQuaDonHang(url:String, params:NSDictionary) -> Void {
+        WebServices().CallGlobalAPI(url: url, headers: [:], parameters:params, httpMethod: "GET", progressView:true, uiView:self.view, networkAlert: true) {(_ jsonResponse:JSON? , _ strErrorMessage:String) in
+            if strErrorMessage.count != 0 {
+                showAlertMessage(titleStr: Bundle.main.displayName!, messageStr: strErrorMessage)
+            }
+            else {
+                print(jsonResponse!)
+                let responseCode = jsonResponse!["result"].stringValue
+                if responseCode == "success" {
+                     let hieu_qua_don_hang = jsonResponse!["data"].dictionaryValue
+                     
+                    let entry1 = PieChartDataEntry(value: Double(24), label: "#1")
+                              let entry2 = PieChartDataEntry(value: Double(49), label: "#2")
+                              let entry3 = PieChartDataEntry(value: Double(32), label: "#3")
+                           let dataSet = PieChartDataSet(entries: [entry1, entry2, entry3], label: "Widget Types")
+                              let data = PieChartData(dataSet: dataSet)
+                    self.pieChart.data = data
+                              self.pieChart.chartDescription?.text = "Share of Widgets by Type"
+
+                              // Color
+                              dataSet.colors = ChartColorTemplates.joyful()
+                              //dataSet.valueColors = [UIColor.black]
+                              self.pieChart.backgroundColor = UIColor.white
+                              self.pieChart.holeColor = UIColor.clear
+                              self.pieChart.chartDescription?.textColor = UIColor.black
+                              self.pieChart.legend.textColor = UIColor.black
+                              
+                              // Text
+                              self.pieChart.legend.font = UIFont(name: "Futura", size: 10)!
+                              self.pieChart.chartDescription?.font = UIFont(name: "Futura", size: 12)!
+                              self.pieChart.chartDescription?.xOffset = self.pieChart.frame.width
+                              self.pieChart.chartDescription?.yOffset = self.pieChart.frame.height * (2/3)
+                              self.pieChart.chartDescription?.textAlign = NSTextAlignment.left
+
+                              // Refresh chart with new data
+                              self.pieChart.notifyDataSetChanged()
+                    
+                    
+                }
+                else {
+                    showAlertMessage(titleStr: Bundle.main.displayName!, messageStr: jsonResponse!["message"].stringValue)
+                }
+            }
+        }
+    }
+    
     
 }
 
