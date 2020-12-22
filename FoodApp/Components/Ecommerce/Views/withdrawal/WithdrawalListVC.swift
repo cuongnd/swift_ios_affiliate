7c444085a3c56651c24401484e401800f7b28039
cuongnd <nguyendinhcuong@gmail.com>
2020-12-22 08:31:41 +0700

    update lich su rut tien

--- FoodApp/Components/Ecommerce/Views/withdrawal/WithdrawalListVC.swift
+++ FoodApp/Components/Ecommerce/Views/withdrawal/WithdrawalListVC.swift
@@ -8,19 +8,25 @@
 
 import UIKit
 import SwiftyJSON
+import SwiftDataTables
 
-class Withdrawal: UITableViewCell {
-    
-
-}
 
 
 class WithdrawalListVC: UIViewController {
     var OrderHistoryData = [JSON]()
     var selected = String()
+    lazy var dataTable = makeDataTable()
+    var dataSource: DataTableContent = []
+    @IBOutlet weak var UIViewLichSuRutTien: UIView!
+    let headerTitles = ["Name", "Fav Beverage", "Fav language", "Short term goals", "Height"]
     override func viewDidLoad() {
         super.viewDidLoad()
         self.selected = ""
+        
+        setupViews()
+        setupConstraints()
+        
+        
         
     }
     @objc private func refreshData(_ sender: Any) {
@@ -42,7 +48,56 @@ class WithdrawalListVC: UIViewController {
             self.slideMenuController()?.openRight()
         }
     }
+    func setupViews() {
+        automaticallyAdjustsScrollViewInsets = false
+        navigationController?.navigationBar.isTranslucent = false
+        title = "Streaming fans"
+        self.UIViewLichSuRutTien.backgroundColor = UIColor.white
+        dataSource = [
+            [
+                DataTableValueType.string("Pavan"),
+                DataTableValueType.string("Juice"),
+                DataTableValueType.string("Swift and Php"),
+                DataTableValueType.string("Be a game publisher"),
+                DataTableValueType.float(175.25)
+            ],
+            [
+                DataTableValueType.string("NoelDavies"),
+                DataTableValueType.string("Water"),
+                DataTableValueType.string("Php and Javascript"),
+                DataTableValueType.string("'Be a fucking paratrooper machine'"),
+                DataTableValueType.float(185.80)
+            ],
+            [
+                DataTableValueType.string("Redsaint"),
+                DataTableValueType.string("Cheerwine and Dr.Pepper"),
+                DataTableValueType.string("Java"),
+                DataTableValueType.string("'Creating an awesome RPG Game game'"),
+                DataTableValueType.float(185.42)
+            ],
+        ]
+        dataTable.reload()
+        
+        self.UIViewLichSuRutTien.addSubview(dataTable)
+    }
+
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            dataTable.topAnchor.constraint(equalTo: self.UIViewLichSuRutTien.layoutMarginsGuide.topAnchor),
+            dataTable.leadingAnchor.constraint(equalTo: self.UIViewLichSuRutTien.leadingAnchor),
+            dataTable.bottomAnchor.constraint(equalTo: self.UIViewLichSuRutTien.layoutMarginsGuide.bottomAnchor),
+            dataTable.trailingAnchor.constraint(equalTo: self.UIViewLichSuRutTien.trailingAnchor),
+        ])
+    }
+
+    public func addDataSourceAfter(){
+        
+        
+    }
+
 }
+
+
 
 //MARK: WithdrawalList
 extension WithdrawalListVC {
@@ -66,5 +121,36 @@ extension WithdrawalListVC {
                 }
             }
         }
+    }
+}
+extension WithdrawalListVC {
+    private func makeDataTable() -> SwiftDataTable {
+        let dataTable = SwiftDataTable(dataSource: self)
+        dataTable.translatesAutoresizingMaskIntoConstraints = false
+        dataTable.delegate = self
+        return dataTable
+    }
+}
+extension WithdrawalListVC: SwiftDataTableDataSource {
+    public func dataTable(_ dataTable: SwiftDataTable, headerTitleForColumnAt columnIndex: NSInteger) -> String {
+        return self.headerTitles[columnIndex]
+    }
+    
+    public func numberOfColumns(in: SwiftDataTable) -> Int {
+        return 4
+    }
+    
+    func numberOfRows(in: SwiftDataTable) -> Int {
+        return self.dataSource.count
+    }
+    
+    public func dataTable(_ dataTable: SwiftDataTable, dataForRowAt index: NSInteger) -> [DataTableValueType] {
+        return self.dataSource[index]
+    }
+}
+
+extension WithdrawalListVC: SwiftDataTableDelegate {
+    func didSelectItem(_ dataTable: SwiftDataTable, indexPath: IndexPath) {
+        debugPrint("did select item at indexPath: \(indexPath) dataValue: \(dataTable.data(for: indexPath))")
     }
 }
