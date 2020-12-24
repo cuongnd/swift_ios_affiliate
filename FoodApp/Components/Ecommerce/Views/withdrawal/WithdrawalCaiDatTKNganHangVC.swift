08eeafd52e7c52004d8a6bc2ebd21da4b6390ff9
cuongnd <nguyendinhcuong@gmail.com>
2020-12-24 18:45:23 +0700

    UPDATE CODE

--- FoodApp/Components/Ecommerce/Views/withdrawal/WithdrawalCaiDatTKNganHangVC.swift
+++ FoodApp/Components/Ecommerce/Views/withdrawal/WithdrawalCaiDatTKNganHangVC.swift
@@ -13,12 +13,23 @@ import SwiftyJSON
 
 class WithdrawalCaiDatTKNganHangVC: UIViewController {
    
+   @IBOutlet weak var btn_ok: UIButton!
+    @IBOutlet weak var text_viewNote: UITextView!
+    var Note = String()
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        
-        
-        
+        self.text_viewNote.text = Note
+        cornerRadius(viewName: btn_ok, radius: 6.0)
+        cornerRadius(viewName: text_viewNote, radius: 6.0)
+    }
+    
+
+    @IBAction func btnTap_Ok(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func btnTap_dismiss(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
     }
     @objc private func refreshData(_ sender: Any) {
         let user_id:String=UserDefaultManager.getStringFromUserDefaults(key: UD_userId);
